7741cac830ab2a062ed05103e8b2e4f9c0cfa877
Abdelrahman Ahmed <101799276+Abdelrahman-Ahmed-2003@users.noreply.github.com>
2024-05-12 18:26:34 +0300

    put icon

--- Calculator/lib/main.dart
+++ Calculator/lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      title: "Calculator",
       debugShowCheckedModeBanner: false,
       home: Calculator(),
     );
